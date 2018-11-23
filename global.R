6c973c858f399f83b9b2af559e692edb6eb4b97c
hyunsooda <hyunsoo7708@gmail.com>
2018-11-24 03:43:46 +0900

    Update global.R

--- global.R
+++ global.R
@@ -14,11 +14,5 @@ options(shiny.sanitize.errors = FALSE)
 Sys.setlocale("LC_ALL", "ko_KR.UTF-8")
 
 g = make_empty_graph()
-# g = add_vertices(g, 1, name='hello')
-# g = add_vertices(g, 1, name='world')
-# g = add_vertices(g, 1, name='hello2')
-# g = add_vertices(g, 1, name='world2')
-# g = add_edges(g, c('hello', 'world', 'world', 'hello', 'world2','hello2', 'hello2', 'world2'))
-# plot(g, family='AppleMyungjo')
-# gg = g
+
 whichGraph = 0
